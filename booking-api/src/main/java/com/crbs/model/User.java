5905233066bf51985c46df48b66dd44ec9d21cde
JEONGBOYEON <qhdus96@naver.com>
2020-03-02 17:12:53 +0900

    보안구현 하기전 회원가입, 회원탈퇴, 회원정보 수정 부분 구현

---
+++ booking-api/src/main/java/com/crbs/model/User.java
@@ -0,0 +1,44 @@
+package com.crbs.model;
+
+public class User {
+
+	private String id;
+	private String name;
+	private String password;
+	private String phonenumber;
+	private Boolean isadmin;
+	
+	
+	public Boolean getIsadmin() {
+		return isadmin;
+	}
+	public void setIsadmin(Boolean isadmin) {
+		this.isadmin = isadmin;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getPhonenumber() {
+		return phonenumber;
+	}
+	public void setPhonenumber(String phonenumber) {
+		this.phonenumber = phonenumber;
+	}
+	
+	
+}
